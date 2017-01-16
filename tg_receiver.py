930fc06446249a3040f89960fedb2c0a2689ae29
wildy <me@koshaq.net>
2017-01-17 00:15:04 +0300

    PEP8

--- tg_receiver.py
+++ tg_receiver.py
@@ -2,7 +2,10 @@
 # -*- encoding: utf-8 -*-
 #
 # Telegram events receiver, acknowledges events.
-import ConfigParser, telebot, requests, re
+import ConfigParser
+import telebot
+import requests
+import re
 from pyzabbix import ZabbixAPI
 from datetime import datetime
 
@@ -25,42 +28,54 @@ zabbix_api_password = c.get('zabbix', 'zabbix_api_password')
 bot = telebot.TeleBot(api_token)
 
 # Get callback query and process it
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def ack_callback(call):
-	ack = {}
-	ack['eventid']	= int(re.search(r'^ACKNOWLEDGE (\d+)$', call.data).group(1))
-	ack['timestamp'] = datetime.fromtimestamp(int(call.message.date))
-	message = "SmartLabs_Monitoring_Bot: Acknowledged by @%s from Telegram" % call.from_user.username
+    ack = {}
+    ack['eventid'] = int(re.search(r'^ACKNOWLEDGE (\d+)$', call.data).group(1))
+    ack['timestamp'] = datetime.fromtimestamp(int(call.message.date))
+    message = "SmartLabs_Monitoring_Bot: Acknowledged by @%s from Telegram" % call.from_user.username
 
-	# If the message is older than ignore_older_than_mins, complain to chat loudly
-	reply_age_mins = ((datetime.now() - ack['timestamp']).seconds // 60)%60
-	too_old_msg = "*ERROR:* Received ACK to message older than %s mins, you can only ACK this manually!" % ignore_older_than_mins
-	success_msg = "@%s *successfully acknowledged* Zabbix trigger %s." % (call.from_user.username, ack['eventid'])
-	unsuccessful_msg = "*ERROR:* Could not acknowledge Zabbix event %s, check bot!" % ack['eventid']
+    # If the message is older than ignore_older_than_mins, complain to chat
+    # loudly
+    reply_age_mins = ((datetime.now() - ack['timestamp']).seconds // 60) % 60
+    too_old_msg = "*ERROR:* Received ACK to message older than %s mins, you can only ACK this manually!" % ignore_older_than_mins
+    success_msg = "@%s *successfully acknowledged* Zabbix trigger %s." % (
+        call.from_user.username, ack['eventid'])
+    unsuccessful_msg = "*ERROR:* Could not acknowledge Zabbix event %s, check bot!" % ack[
+        'eventid']
 
-	if (reply_age_mins > ignore_older_than_mins):
-		# User clicked the button on an ancient message
-		print(too_old_msg + ', call: ' + str(call))
-		bot.send_message(call.message.chat.id, too_old_msg, parse_mode='Markdown', reply_to_message_id=call.message.message_id)
-		bot.answer_callback_query(call.id, text=too_old_msg, show_alert=True)
-	else:
-		try:
-			# Try to ACK it in Zabbix
-			zabbix_acknowledge(ack['eventid'], message)
-			bot.send_message(call.message.chat.id, success_msg, parse_mode='Markdown', reply_to_message_id=call.message.message_id, disable_notification=True)
-			bot.answer_callback_query(call.id, text=success_msg, show_alert=False)
-		except:
-			# For some reason we couldn't :(
-			bot.send_message(call.message.chat.id, unsuccessful_msg, parse_mode='Markdown')
-			bot.answer_callback_query(call.id, text=unsuccessful_msg, show_alert=True)
-			raise
+    if (reply_age_mins > ignore_older_than_mins):
+        # User clicked the button on an ancient message
+        print(too_old_msg + ', call: ' + str(call))
+        bot.send_message(call.message.chat.id, too_old_msg,
+                         parse_mode='Markdown', reply_to_message_id=call.message.message_id)
+        bot.answer_callback_query(call.id, text=too_old_msg, show_alert=True)
+    else:
+        try:
+            # Try to ACK it in Zabbix
+            zabbix_acknowledge(ack['eventid'], message)
+            bot.send_message(call.message.chat.id, success_msg, parse_mode='Markdown',
+                             reply_to_message_id=call.message.message_id, disable_notification=True)
+            bot.answer_callback_query(
+                call.id, text=success_msg, show_alert=False)
+        except:
+            # For some reason we couldn't :(
+            bot.send_message(call.message.chat.id,
+                             unsuccessful_msg, parse_mode='Markdown')
+            bot.answer_callback_query(
+                call.id, text=unsuccessful_msg, show_alert=True)
+            raise
 
 
 def zabbix_acknowledge(event_id, message):
-	# Try to acknowledge an event in Zabbix
-	zapi = ZabbixAPI(url=zabbix_url, user=zabbix_api_user, password=zabbix_api_password)
-	zapi.do_request('event.acknowledge', {'eventids': event_id, 'message': message })
+    # Try to acknowledge an event in Zabbix
+    zapi = ZabbixAPI(url=zabbix_url, user=zabbix_api_user,
+                     password=zabbix_api_password)
+    zapi.do_request('event.acknowledge', {
+                    'eventids': event_id, 'message': message})
 
 if __name__ == "__main__":
-	# Start the bot
-	bot.polling()
+    # Start the bot
+    bot.polling()
--- tg_sender.py
+++ tg_sender.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
-import ConfigParser, telebot, requests, argparse, os
+import ConfigParser
+import telebot
+import requests
+import argparse
+import os
 from telebot import types
 
 # Change directory to script's current dir
@@ -23,11 +27,12 @@ zabbix_description = c.get('zabbix', 'zabbix_description')
 # Init bot
 bot = telebot.TeleBot(api_token)
 
+
 @bot.message_handler(content_types=["text"])
 def handle_trigger(event_id, trigger, severity, status, group, zabbix_description, level=None):
-	from telebot import types
-	group_id = c.get('groups', group)
-	message = """
+    from telebot import types
+    group_id = c.get('groups', group)
+    message = """
 <b>%s</b>
 (%s)
 Trigger <b>%s</b>:
@@ -35,36 +40,42 @@ Trigger <b>%s</b>:
 %s
 	""" % (zabbix_description, severity, status, trigger)
 
-	# If passed level from command line (-l), override config
-	if not level:
-		level = c.get('severities', severity)
+    # If passed level from command line (-l), override config
+    if not level:
+        level = c.get('severities', severity)
 
-	# Do not show notifications if notification level is set low for this event (by config or '-l' parameter)
-	if level < -9:
-		# Don't even bother sending it
-		return
-	elif level < 0:
-		disable_notification = True
-	else:
-		disable_notification = False
-	print(disable_notification)
-	kb = types.InlineKeyboardMarkup()
+    # Do not show notifications if notification level is set low for this
+    # event (by config or '-l' parameter)
+    if level < -9:
+        # Don't even bother sending it
+        return
+    elif level < 0:
+        disable_notification = True
+    else:
+        disable_notification = False
+    print(disable_notification)
+    kb = types.InlineKeyboardMarkup()
 
-	if status == 'PROBLEM':
-		ack_button = types.InlineKeyboardButton(text="Acknowledge", callback_data="ACKNOWLEDGE %s" % event_id)
-		kb.add(ack_button)
+    if status == 'PROBLEM':
+        ack_button = types.InlineKeyboardButton(
+            text="Acknowledge", callback_data="ACKNOWLEDGE %s" % event_id)
+        kb.add(ack_button)
 
-	bot.send_message(group_id, message, parse_mode='HTML', disable_notification=disable_notification, reply_markup=kb)
+    bot.send_message(group_id, message, parse_mode='HTML',
+                     disable_notification=disable_notification, reply_markup=kb)
 
 parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument('-i', dest='event_id', help='Event ID (for inline acknowledge)', required=True, type=int)
+parser.add_argument('-i', dest='event_id',
+                    help='Event ID (for inline acknowledge)', required=True, type=int)
 parser.add_argument('-t', dest='trigger', help='Trigger name', required=True)
 parser.add_argument('-s', dest='severity', help='Severity', required=True)
 parser.add_argument('-S', dest='status', help='Status', required=True)
-parser.add_argument('-g', dest='group', help='Group to send trigger to', required=True)
-parser.add_argument('-l', dest='level', help='Override this event notification level: -2 = do not send at all; -1 = send without sound, 0 = send standard notification', type=int, default=None)
+parser.add_argument('-g', dest='group',
+                    help='Group to send trigger to', required=True)
+parser.add_argument('-l', dest='level',
+                    help='Override this event notification level: -2 = do not send at all; -1 = send without sound, 0 = send standard notification', type=int, default=None)
 
 args = parser.parse_args()
 
-handle_trigger(args.event_id, args.trigger, args.severity, args.status, args.group, zabbix_description, args.level)
-
+handle_trigger(args.event_id, args.trigger, args.severity,
+               args.status, args.group, zabbix_description, args.level)
